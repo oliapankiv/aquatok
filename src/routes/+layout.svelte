755461fcfb7999d78caa03f7d0d49987a1536d8f
Andrew Nyzhnyk <andrew@nyzhnyk.com>
2025-08-28 19:23:08 +0200

    ft: favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+  import { _ } from 'svelte-i18n'
+
   import '../app.css'
-  import favicon from '$lib/assets/favicon.svg'
+  import faviconsvg from '$lib/assets/favicon/favicon.svg'
+  import faviconpng from '$lib/assets/favicon/favicon.png'
 
   let { children } = $props()
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  <link rel="icon" href={faviconsvg} type="image/svg+xml" />
+  <link rel="icon" href={faviconpng} type="image/png" sizes="64x64" />
+  <title>{$_('title.default')}</title>
 </svelte:head>
 
 {@render children?.()}
