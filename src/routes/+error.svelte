45099ec27b0c171ad7efc3cee4f42861581e22eb
Andrew Nyzhnyk <andrew@nyzhnyk.com>
2025-08-28 19:04:40 +0200

    ref: i18n - locale key

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,10 @@
 <script>
+  import { _ } from 'svelte-i18n'
   import { page } from '$app/state'
 </script>
 
 <svelte:head>
-  <title>Not Found</title>
+  <title>{$_('title.404')}</title>
 </svelte:head>
 
 <h1>{page?.error?.message}</h1>
