35f6afe92884c53545f61a063f5b2b45ca3817e2
Olha Pankiv <oliapankiv13@gmail.com>
2025-09-08 20:56:42 +0300

    ft: footer

---
+++ src/lib/ui/icons/Facebook.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements'
+
+  const props: HTMLAttributes<SVGElement> = $props()
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="currentColor" {...props}>
+  <path
+    d="M64 32.1947C64 14.4097 49.6774 0 32 0C14.3226 0 0 14.4097 0 32.1947C0 48.2635 11.7019 61.5828 27 64V41.5013H18.871V32.1947H27V25.1015C27 17.0333 31.7742 12.5767 39.0865 12.5767C42.5884 12.5767 46.2503 13.205 46.2503 13.205V21.1239H42.2142C38.24 21.1239 37 23.606 37 26.1517V32.1947H45.8748L44.4555 41.5013H37V64C52.2981 61.5828 64 48.2635 64 32.1947Z"
+  />
+</svg>
