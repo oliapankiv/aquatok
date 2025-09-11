fcc248620e20cd235cf6381351547249b84c2e8f
Olha Pankiv <oliapankiv13@gmail.com>
2025-09-11 20:37:30 +0300

    ft: hero banner

---
+++ src/lib/ui/molecules/Services.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n'
+
+  import type { HTMLAttributes } from 'svelte/elements'
+
+  const { class: className, ...props }: HTMLAttributes<HTMLElement> = $props()
+</script>
+
+<section class={['h-100 w-full bg-neutral-200', className]} {...props}>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8"></div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n'
+
+  import Hero from '$lib/ui/molecules/Hero.svelte'
+  import Services from '$lib/ui/molecules/Services.svelte'
 </script>
+
+<Hero />
+
+<Services />
 
 <h1>{$_('')}</h1>
