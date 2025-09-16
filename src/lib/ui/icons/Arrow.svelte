2677f813b9e73fbf3e2505697810a26b1774d26b
Olha Pankiv <oliapankiv13@gmail.com>
2025-09-16 18:15:30 +0300

    ft: faq section

---
+++ src/lib/ui/icons/Arrow.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements'
+
+  const props: HTMLAttributes<SVGElement> = $props()
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" stroke="currentColor" {...props}>
+  <path d="M4 6.5L8 10.5L12 6.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import Hero from '$lib/ui/molecules/Hero.svelte'
   import Contact from '$lib/ui/molecules/Contact.svelte'
+  import Faq from '$lib/ui/molecules/Faq.svelte'
 </script>
 
 <Hero />
 
 <Contact />
+
+<Faq />
