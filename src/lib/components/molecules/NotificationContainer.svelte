a373baa8899a350dede6277cf1edffacff21d6df
Andrew Nyzhnyk <andrew@nyzhnyk.com>
2025-10-11 17:02:36 +0200

    ft: form validation

--- src/lib/components/molecules/NotificationContainer.svelte
+++ src/lib/components/molecules/NotificationContainer.svelte
@@ -4,7 +4,7 @@
   import Notification from '$lib/ui/atoms/Notification.svelte'
 </script>
 
-<div class="absolute top-0 right-0 z-50 flex w-full flex-col gap-3 p-4 sm:px-6 sm:py-8 md:w-xl">
+<div class="fixed top-0 right-0 z-50 flex w-full flex-col gap-3 p-4 sm:px-6 sm:py-8 md:w-xl">
   {#each $notifications as notification}
     <Notification {notification} />
   {/each}
