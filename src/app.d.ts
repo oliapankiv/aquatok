fd50fd7a2dd465b73ced0f8f04fc931d9bf190a0
Andrew Nyzhnyk <andrew@nyzhnyk.com>
2025-10-15 19:41:06 +0200

    ft: captcha

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,12 @@
+import type { ClientCaptcha } from '$lib/types/captcha'
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
+  interface Window {
+    grecaptcha: ClientCaptcha
+  }
+
   namespace App {
     // interface Error {}
     // interface Locals {}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,11 +3,12 @@ import { fail } from '@sveltejs/kit'
 import { isValidPhoneNumber } from 'libphonenumber-js'
 
 import { PUBLIC_ADAPTER } from '$env/static/public'
-import { PRIVATE_BOT_KEY, PRIVATE_GROUP_ID, PRIVATE_THREAD_ID } from '$env/static/private'
+import { PRIVATE_BOT_KEY, PRIVATE_CAPTCHA, PRIVATE_GROUP_ID, PRIVATE_THREAD_ID } from '$env/static/private'
 
 import { Adapter } from '$lib/enums'
 
 import type { Actions } from './$types'
+import type { ServerCaptcha } from '$lib/types/captcha'
 
 const IS_ENABLED = (PUBLIC_ADAPTER === Adapter.NODE) as unknown as true
 
@@ -16,12 +17,31 @@ const bot = new Bot(PRIVATE_BOT_KEY)
 const makeMessage = (name: string, phone: string, issue: string): string =>
   `🚨 *Запит на контакт:*\nІм'я:\` \`\`${name}\`\nТелефон:\` \`\`${phone}\`\nПроблема:\` \`\`${issue}\``
 
+const isCaptchaInvalid = async (token: string): Promise<boolean> => {
+  const formData = new FormData()
+  formData.set('response', token)
+  formData.set('secret', PRIVATE_CAPTCHA)
+
+  const result = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((result): Promise<ServerCaptcha> => result.json())
+    .catch(() => undefined)
+
+  return !result || !result.success || result.score < 0.4
+}
+
 export const actions =
   IS_ENABLED &&
   ({
     default: async ({ request }) => {
       const formData = await request.formData().catch(() => undefined)
       if (!formData) return fail(400, { errorMessage: 'section.contactUsForm.error.nodata' })
+
+      const captcha = formData.get('captcha')
+      if (typeof captcha !== 'string' || (await isCaptchaInvalid(captcha)))
+        return fail(400, { errorMessage: 'section.contactUsForm.error.captcha' })
 
       const name = formData.get('name')
       if (typeof name !== 'string' || name.length < 2) return fail(400, { errorMessage: 'section.contactUsForm.name.error' })
